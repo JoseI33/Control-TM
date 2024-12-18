680bde8ab3994fe964cb5e18350d859f7336d690
JoseI33 <jose.imhoff.20,2@gmail.com>
2024-12-18 15:13:46 -0300

    Visualmente terminado la pagina de repuestos

--- Control-TM/src/Page/Productos/index.jsx
+++ Control-TM/src/Page/Productos/index.jsx
@@ -4,15 +4,39 @@ import Card from "../../components/Card"
 const Product = () => {
     
    return (
-    <div className='container'>
-        <h1 className='heanding'>Repuestos</h1>
-        <div className='grid-container'>
-        <Card title='Correas' description='Stock de correas del depósito.' />
-        <Card title='Mangueras'description='Stock de mangueras del depósito.' />
-        <Card title='Rodamientos'description='Stock de rodamientos del depósito.' />
-        <Card title='Retenes'description='Stock de retenes del depósito.' />
-        <Card title='Filtros'description='Stock de filtros del depósito.' />
-        <Card title='Torneria'description='Stock de torneria del depósito.' />
+    <div className='product-container'>
+        <h1 className='product-title'>Repuestos</h1>
+        <div className='product-grid'>
+        <Card 
+        title='Correas' 
+        description='Stock de correas.'
+        imageURL={'https://res.cloudinary.com/dsolw4c9p/image/upload/v1734544952/Productos-Correas-Industriales-Gates-00_wsbtbw.jpg'} 
+        />
+        <Card 
+        title='Mangueras'
+        description='Stock de mangueras.' 
+        imageURL={'https://res.cloudinary.com/dsolw4c9p/image/upload/v1734545024/R_mroahp.jpg'}
+        />
+        <Card 
+        title='Rodamientos'
+        description='Stock de rodamientos.' 
+        imageURL={'https://res.cloudinary.com/dsolw4c9p/image/upload/v1734545146/OIP_gyv84r.jpg'}
+        />
+        <Card 
+        title='Retenes'
+        description='Stock de retenes.' 
+        imageURL={'https://res.cloudinary.com/dsolw4c9p/image/upload/v1734545232/retenes-brr-min_t2pqfe.jpg'}
+        />
+        <Card 
+        title='Filtros'
+        description='Stock de filtros.' 
+        imageURL={'https://res.cloudinary.com/dsolw4c9p/image/upload/v1734545413/OIP_1_u14rdi.jpg'}
+        />
+        <Card 
+        title='Torneria'
+        description='Stock de torneria.' 
+        imageURL={'https://res.cloudinary.com/dsolw4c9p/image/upload/v1734545545/icono-torno-isometrico-icono-vector-torno-diseno-web-aislado-sobre-fondo-blanco_96318-55390_z9dypv.jpg'}
+        />
         </div>
     </div>
 
--- Control-TM/src/components/Card/index.jsx
+++ Control-TM/src/components/Card/index.jsx
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types'
 const Card = ({title, description, imageURL}) => {
 
     return (
-        <div className='card'>
-            <img className='' src={imageURL} alt=''/>
-            <h2 className='card-title'>{title}</h2>
+        <div className="card-conteiner">
+            <div className='card'>
+                <img className='card-image' src={imageURL} alt=''/>
+                <h2 className='card-title'>{title}</h2>
+            </div>
             <p className='card-description'>{description}</p>
         </div>
     )
@@ -15,6 +17,7 @@ const Card = ({title, description, imageURL}) => {
 Card.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    imageURL: PropTypes.string.isRequired,
 }
 
 export default Card
